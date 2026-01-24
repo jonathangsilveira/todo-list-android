453624a0008b6377f3249f465bfa2fd1a5f251a2
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2026-01-24 17:55:57 -0300

    feature: unit tests for AuthApiService

---
+++ app/src/test/java/org/jgsilveira/todolist/android/features/auth/service/AuthApiServiceStubs.kt
@@ -0,0 +1,100 @@
+package org.jgsilveira.todolist.android.features.auth.service
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.request
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.headersOf
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+private const val APPLICATION_JSON = "application/json"
+
+internal object AuthApiServiceStubs {
+
+    val successResponseSignUpEngine = MockEngine {
+        respond(
+            content = "{\"message\": \"User created!\"}",
+            status = HttpStatusCode.Created,
+            headers = headersOf(
+                HttpHeaders.ContentType,
+                APPLICATION_JSON
+            )
+        )
+    }
+
+    val conflictResponseSignUpEngine = MockEngine {
+        respond(
+            content = "{\"message\": \"User with e-mail chablau@chablau.com already exists!\"}",
+            status = HttpStatusCode.Conflict,
+            headers = headersOf(
+                HttpHeaders.ContentType,
+                APPLICATION_JSON
+            )
+        )
+    }
+
+    val successResponseSignInEngine = MockEngine {
+        respond(
+            content = "{\"refresh_token\": \"token\",\"access_token\": \"token\",\"expires_at\": 0,\"token_type\": \"Bearer\"}",
+            status = HttpStatusCode.OK,
+            headers = headersOf(
+                HttpHeaders.ContentType,
+                APPLICATION_JSON
+            )
+        )
+    }
+
+    val notFoundResponseSignInEngine = MockEngine {
+        respond(
+            content = "{\"message\": \"User with email chablau@chablau.com was not found.\"}",
+            status = HttpStatusCode.NotFound,
+            headers = headersOf(
+                HttpHeaders.ContentType,
+                APPLICATION_JSON
+            )
+        )
+    }
+
+    val noContentResponseSignOutEngine = MockEngine {
+        respond(
+            content = "",
+            status = HttpStatusCode.NoContent,
+            headers = headersOf(
+                HttpHeaders.ContentType,
+                APPLICATION_JSON
+            )
+        )
+    }
+
+    val unauthorizedResponseSignOutEngine = MockEngine {
+        respond(
+            content = "{\"message\": \"Token expired\"}",
+            status = HttpStatusCode.Unauthorized,
+            headers = headersOf(
+                HttpHeaders.ContentType,
+                APPLICATION_JSON
+            )
+        )
+    }
+
+    fun createKtorClient(engine: HttpClientEngine): HttpClient {
+        return HttpClient(engine = engine) {
+            expectSuccess = true
+            install(plugin = ContentNegotiation) {
+                json(
+                    Json {
+                        encodeDefaults = true
+                        isLenient = true
+                        coerceInputValues = true
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+        }
+    }
+}
---
+++ app/src/test/java/org/jgsilveira/todolist/android/features/auth/service/AuthApiServiceTest.kt
@@ -0,0 +1,143 @@
+package org.jgsilveira.todolist.android.features.auth.service
+
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.http.HttpStatusCode
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.jgsilveira.todolist.android.features.auth.data.model.TokenType
+import org.jgsilveira.todolist.android.features.auth.data.service.KtorAuthApiService
+import org.jgsilveira.todolist.android.features.auth.domain.model.SignInForm
+import org.jgsilveira.todolist.android.features.auth.domain.model.SignUpForm
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class AuthApiServiceTest {
+    private val testDispatcher = StandardTestDispatcher(
+        scheduler = TestCoroutineScheduler()
+    )
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun signUpShouldReturnOKWhenFormIsCorrect() = runTest {
+        // Given
+        val engine = AuthApiServiceStubs.successResponseSignUpEngine
+        val httpClient = AuthApiServiceStubs.createKtorClient(engine)
+        val apiService = KtorAuthApiService(httpClient)
+        val form = SignUpForm(
+            fullName = "Chablau",
+            email = "chablau@chablau.com",
+            password = "chablau"
+        )
+
+        // When
+        apiService.signUp(form)
+    }
+
+    @Test
+    fun signUpShouldReturnConflictWhenEmailAlreadyExists() = runTest {
+        // Given
+        val engine = AuthApiServiceStubs.conflictResponseSignUpEngine
+        val httpClient = AuthApiServiceStubs.createKtorClient(engine)
+        val apiService = KtorAuthApiService(httpClient)
+        val form = SignUpForm(
+            fullName = "Chablau",
+            email = "chablau@chablau.com",
+            password = "chablau"
+        )
+
+        // When
+        val exception = assertFailsWith(ClientRequestException::class) {
+            apiService.signUp(form)
+        }
+
+        // Then
+        assert(exception.response.status == HttpStatusCode.Conflict)
+    }
+
+    @Test
+    fun signInShouldReturnOKWhenFormIsCorrect() = runTest {
+        // Given
+        val engine = AuthApiServiceStubs.successResponseSignInEngine
+        val httpClient = AuthApiServiceStubs.createKtorClient(engine)
+        val apiService = KtorAuthApiService(httpClient)
+        val form = SignInForm(
+            username = "chablau@chablau.com",
+            password = "chablau"
+        )
+
+        // When
+        val authTokens = apiService.signIn(form)
+
+        assertEquals(expected = "token", actual = authTokens.refreshToken)
+        assertEquals(expected = "token", actual = authTokens.accessToken)
+        assertEquals(expected = 0, actual = authTokens.expiresAt)
+        assertEquals(expected = TokenType.BEARER, actual = authTokens.type)
+    }
+
+    @Test
+    fun signUpShouldReturnNotFoundWhenUserDoesntExist() = runTest {
+        // Given
+        val engine = AuthApiServiceStubs.notFoundResponseSignInEngine
+        val httpClient = AuthApiServiceStubs.createKtorClient(engine)
+        val apiService = KtorAuthApiService(httpClient)
+        val form = SignInForm(
+            username = "chablau@chablau.com",
+            password = "chablau"
+        )
+
+        // When
+        val exception = assertFailsWith(ClientRequestException::class) {
+            apiService.signIn(form)
+        }
+
+        // Then
+        assert(exception.response.status == HttpStatusCode.NotFound)
+    }
+
+    @Test
+    fun signOutShouldReturnNoContentWhenAccessTokenIsValid() = runTest {
+        // Given
+        val engine = AuthApiServiceStubs.noContentResponseSignOutEngine
+        val httpClient = AuthApiServiceStubs.createKtorClient(engine)
+        val apiService = KtorAuthApiService(httpClient)
+
+        // When
+        apiService.signOut()
+
+        // Then
+    }
+
+    @Test
+    fun signOutShouldReturnUnauthorizedWhenAccessTokenIsExpired() = runTest {
+        // Given
+        val engine = AuthApiServiceStubs.unauthorizedResponseSignOutEngine
+        val httpClient = AuthApiServiceStubs.createKtorClient(engine)
+        val apiService = KtorAuthApiService(httpClient)
+
+        // When
+        val exception = assertFailsWith(ClientRequestException::class) {
+            apiService.signOut()
+        }
+
+        // Then
+        assert(exception.response.status == HttpStatusCode.Unauthorized)
+    }
+}

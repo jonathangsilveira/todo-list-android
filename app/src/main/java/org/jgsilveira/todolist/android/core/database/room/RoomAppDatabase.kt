841b44dbe1aaa16d49217819ecf565fb6e04ac0a
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2026-01-27 18:52:42 -0300

    add: implement RoomDatabase

---
+++ app/src/main/java/org/jgsilveira/todolist/android/core/database/room/RoomAppDatabase.kt
@@ -0,0 +1,26 @@
+package org.jgsilveira.todolist.android.core.database.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import org.jgsilveira.todolist.android.core.database.room.dao.TodoListItemDao
+import org.jgsilveira.todolist.android.core.database.room.entity.TodoListItemEntity
+
+@Database(entities = [TodoListItemEntity::class], version = 1)
+abstract class RoomAppDatabase : RoomDatabase() {
+    abstract fun todoListItemDao(): TodoListItemDao
+
+    companion object {
+        fun from(appContext: Context, databaseName: String): RoomAppDatabase {
+            val databaseBuilder = Room.databaseBuilder(
+                context = appContext,
+                klass = RoomAppDatabase::class.java,
+                name = databaseName
+            )
+            return databaseBuilder
+                .fallbackToDestructiveMigration(dropAllTables = true)
+                .build()
+        }
+    }
+}

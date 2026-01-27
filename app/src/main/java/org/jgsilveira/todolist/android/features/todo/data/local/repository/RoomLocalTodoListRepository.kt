b4565e5513b1e6ce746d43730c9049d38ce21d8d
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2026-01-27 18:54:01 -0300

    add: implement room LocalTodoListRepository

---
+++ app/src/main/java/org/jgsilveira/todolist/android/features/todo/data/local/repository/RoomLocalTodoListRepository.kt
@@ -0,0 +1,35 @@
+package org.jgsilveira.todolist.android.features.todo.data.local.repository
+
+import org.jgsilveira.todolist.android.core.database.room.dao.TodoListItemDao
+import org.jgsilveira.todolist.android.features.todo.data.local.mapper.toDomain
+import org.jgsilveira.todolist.android.features.todo.data.local.mapper.toEntity
+import org.jgsilveira.todolist.android.features.todo.domain.model.TodoListItem
+import org.jgsilveira.todolist.android.features.todo.domain.model.TodoListItemStatus
+import org.jgsilveira.todolist.android.features.todo.domain.repository.LocalTodoListRepository
+
+internal class RoomLocalTodoListRepository(
+    private val dao: TodoListItemDao
+): LocalTodoListRepository {
+    override suspend fun addItem(item: TodoListItem) {
+        val entity = item.toEntity()
+        dao.addItem(entity)
+    }
+
+    override suspend fun updateItem(item: TodoListItem) {
+        val entity = item.toEntity()
+        dao.updateItem(entity)
+    }
+
+    override suspend fun removeItemByUuid(uuid: String) {
+        dao.deleteItemByUuid(uuid)
+    }
+
+    override suspend fun getActiveItems(): List<TodoListItem> {
+        val statuses = listOf(
+            TodoListItemStatus.PENDING.name,
+            TodoListItemStatus.DONE.name
+        )
+        return dao.getActiveItems(statuses = statuses)
+            .map { it.toDomain() }
+    }
+}

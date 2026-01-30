0d5c1d013d8415db1419474d75768aa217312144
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2026-01-30 15:15:57 -0300

    refactor: add pending sync request entity and dao to RoomAppDatabase

--- app/src/main/java/org/jgsilveira/todolist/android/core/database/room/RoomAppDatabase.kt
+++ app/src/main/java/org/jgsilveira/todolist/android/core/database/room/RoomAppDatabase.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import org.jgsilveira.todolist.android.core.database.room.dao.PendingSyncRequestDao
 import org.jgsilveira.todolist.android.core.database.room.dao.TodoListItemDao
+import org.jgsilveira.todolist.android.core.database.room.entity.PendingSyncRequestEntity
 import org.jgsilveira.todolist.android.core.database.room.entity.TodoListItemEntity
 
-@Database(entities = [TodoListItemEntity::class], version = 1)
+@Database(entities = [TodoListItemEntity::class, PendingSyncRequestEntity::class], version = 1)
 abstract class RoomAppDatabase : RoomDatabase() {
     abstract fun todoListItemDao(): TodoListItemDao
+    abstract fun pendingSyncRequestDao(): PendingSyncRequestDao
 
     companion object {
         fun from(appContext: Context, databaseName: String): RoomAppDatabase {

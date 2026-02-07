2c9f8903988fa40f7e4313de6ab2f39d9965e9ff
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2026-02-07 18:14:35 -0300

    refactor: update unit tests to reflect use case changes

--- app/src/test/java/org/jgsilveira/todolist/android/features/todo/domain/RemoveItemByUuidUseCaseTest.kt
+++ app/src/test/java/org/jgsilveira/todolist/android/features/todo/domain/RemoveItemByUuidUseCaseTest.kt
@@ -9,7 +9,9 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.jgsilveira.todolist.android.coroutines.rules.MainDispatcherRule
+import org.jgsilveira.todolist.android.features.todo.domain.model.TodoListRemoteSyncType
 import org.jgsilveira.todolist.android.features.todo.domain.repository.LocalTodoListRepository
+import org.jgsilveira.todolist.android.features.todo.domain.usecase.EnqueueItemChangesRemoteSyncRequestUseCase
 import org.jgsilveira.todolist.android.features.todo.domain.usecase.RemoveItemByUuidUseCase
 import org.junit.Rule
 import org.junit.Test
@@ -22,45 +24,53 @@ internal class RemoveItemByUuidUseCaseTest {
 
     private val localTodoListRepositoryMock = mockk<LocalTodoListRepository>()
 
+    private val enqueueItemChangesRequestMock = mockk<EnqueueItemChangesRemoteSyncRequestUseCase>()
+
     private val removeItemByUuidUseCase = RemoveItemByUuidUseCase(
         localTodoListRepository = localTodoListRepositoryMock,
+        enqueueItemChangesRequest = enqueueItemChangesRequestMock,
         coroutineDispatcher = UnconfinedTestDispatcher()
     )
 
     @Test
     fun `invoke Should return Success When repository succeeds`() = runTest {
         // Given
-        val uuid = "valid-uuid"
+        val item = TodoListItemStubs.pendingTodoListItem
+        val remoteSyncType = TodoListRemoteSyncType.REMOVE_ITEM
         coEvery {
-            localTodoListRepositoryMock.removeItemByUuid(uuid)
+            localTodoListRepositoryMock.removeItemByUuid(item.uuid)
+        } just runs
+        coEvery {
+            enqueueItemChangesRequestMock.invoke(item, remoteSyncType)
         } just runs
 
         // When
-        val result = removeItemByUuidUseCase(uuid)
+        val result = removeItemByUuidUseCase(item)
 
         // Then
         assert(result.isSuccess)
         coVerify {
-            localTodoListRepositoryMock.removeItemByUuid(uuid)
+            localTodoListRepositoryMock.removeItemByUuid(item.uuid)
+            enqueueItemChangesRequestMock.invoke(item, remoteSyncType)
         }
     }
 
     @Test
     fun `invoke Should return Failure When repository fails`() = runTest {
         // Given
-        val uuid = "invalid-uuid"
+        val item = TodoListItemStubs.pendingTodoListItem
         coEvery {
-            localTodoListRepositoryMock.removeItemByUuid(uuid)
+            localTodoListRepositoryMock.removeItemByUuid(item.uuid)
         } throws Exception("something went wrong")
 
         // When
-        val result = removeItemByUuidUseCase(uuid)
+        val result = removeItemByUuidUseCase(item)
 
         // Then
         assert(result.isFailure)
         assertEquals("something went wrong", result.exceptionOrNull()?.message)
         coVerify {
-            localTodoListRepositoryMock.removeItemByUuid(uuid)
+            localTodoListRepositoryMock.removeItemByUuid(item.uuid)
         }
     }
 }

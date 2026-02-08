df36cac5c66d9744e634a3d00c2a2d0ac3b0f42c
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2026-02-08 19:37:29 -0300

    refactor: replace error call with IllegalArgumentException

--- app/src/main/java/org/jgsilveira/todolist/android/features/todo/data/remote/sync/AddTodoListItemRemoteSyncStrategy.kt
+++ app/src/main/java/org/jgsilveira/todolist/android/features/todo/data/remote/sync/AddTodoListItemRemoteSyncStrategy.kt
@@ -5,6 +5,7 @@ import org.jgsilveira.todolist.android.features.sync.remote.api.sync.RemoteSyncS
 import org.jgsilveira.todolist.android.features.todo.data.remote.client.TodoListClient
 import org.jgsilveira.todolist.android.features.todo.data.remote.mapper.toDomain
 import org.jgsilveira.todolist.android.features.todo.domain.repository.LocalTodoListRepository
+import kotlin.IllegalArgumentException
 
 class AddTodoListItemRemoteSyncStrategy internal constructor(
     private val todoListClient: TodoListClient,
@@ -12,8 +13,9 @@ class AddTodoListItemRemoteSyncStrategy internal constructor(
     private val decoder: TodoListItemDecoder
 ): RemoteSyncStrategy {
     override suspend fun sync(entityId: String, payload: String?) {
-        val payload = payload
-            ?: error("Payload to $entityId must not be null to add TODO list items remotely")
+        val payload = payload ?: throw IllegalArgumentException(
+            "Payload to $entityId must not be null to add TODO list items remotely"
+        )
         val requestBody = decoder.decode(payload)
         val response = todoListClient.addItem(requestBody)
         val createdItem = response.toDomain()
